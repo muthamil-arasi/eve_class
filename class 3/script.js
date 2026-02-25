9378c214502523939ce8586f7ba4f7eab988a7f8
muthamil-arasi <muthamil18032007@gmail.com>
2026-02-25 17:03:09 +0530

    k problem

---
+++ class 3/script.js
@@ -0,0 +1,15 @@
+// change the values to the last according to the k value
+
+// let arrays=[1,2,3,4,5];
+// let k=2;
+// for(let i=1;i<=arrays.length;i++){
+//     if(k==)
+// }
+
+
+let arr = [1, 2, 3, 4, 5];
+let n = 3;
+
+let result = arr.slice(n).concat(arr.slice(0, n));
+
+console.log(result);
